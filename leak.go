f570cce12180cbb686036e955b326f87a6cae143
wunderbarb <eric@eric-diehl.com>
2024-07-05 18:33:25 -0700

    Adding new `NoLeak` and `NoLeakButPersistentHTTP`

---
+++ leak.go
@@ -0,0 +1,31 @@
+// v0.1.0
+// Author: DIEHL E.
+// (©) Sony Pictures Entertainment, Apr 2022
+
+package test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/ysmood/gotrace"
+)
+
+// NoLeak verifies whether there was no new running go routines after the current test.
+func NoLeak(t *testing.T) {
+	ign := gotrace.IgnoreCurrent()
+	gotrace.CheckLeak(t, 0, ign)
+}
+
+// NoLeakButPersistentHTTP verifies whether there was no new running go routines after the current test excepted
+// the ones that HTTP may create for a persistent connection
+func NoLeakButPersistentHTTP(t *testing.T) {
+	ign := gotrace.CombineIgnores(
+		gotrace.IgnoreCurrent(),
+		func(t *gotrace.Trace) bool {
+			return strings.Contains(t.Raw, "net/http.(*persistConn).writeLoop")
+		},
+		gotrace.IgnoreFuncs("internal/poll.runtime_pollWait"),
+	)
+	gotrace.CheckLeak(t, 0, ign)
+}
